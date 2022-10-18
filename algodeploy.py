c142956b0b674601b9c04dfd9fa6c03f581a5ad8
Joe Polny <joepolny@gmail.com>
2022-10-18 10:13:20 -0400

    windows install

--- algodeploy.py
+++ algodeploy.py
@@ -38,15 +38,21 @@ class AlgoDeploy:
         tarball_path = Path.joinpath(self.download_dir, tarball)
         url = f'https://github.com/algorand/go-algorand/releases/download/{version_string}/{tarball}'
 
-        self.download_url(url, tarball_path)
+        shutil.rmtree(path=self.localnet_dir, ignore_errors=True)
 
-        print("Extracting node software...")
-        file = tarfile.open(tarball_path)
-        
-        file.extractall(path=self.localnet_dir)
-        shutil.rmtree(Path.joinpath(self.localnet_dir, "data"))
-        shutil.rmtree(Path.joinpath(self.localnet_dir, "genesis"))
-        shutil.rmtree(Path.joinpath(self.localnet_dir, "test-utils"))
+        try:
+            self.download_url(url, tarball_path)
+            print("Extracting node software...")
+            file = tarfile.open(tarball_path)
+            file.extractall(path=self.localnet_dir)
+            file.close()
+            shutil.rmtree(Path.joinpath(self.localnet_dir, "data"))
+            shutil.rmtree(Path.joinpath(self.localnet_dir, "genesis"))
+            shutil.rmtree(Path.joinpath(self.localnet_dir, "test-utils"))
+
+        except:
+            print("Failed to download node software. Building from source...")
+            self.build_from_source(version_string)
 
         template_path = Path.joinpath(self.download_dir, "template.json")
         goal_path = Path.joinpath(self.bin_dir, "goal")
@@ -58,8 +64,6 @@ class AlgoDeploy:
         self.cmd(f'{goal_path} network create --network localnet --template {template_path} --rootdir {self.data_dir}')
         self.cmd(f'{goal_path} -d {self.data_dir} node start')
         self.cmd(f'{goal_path} -d {self.data_dir} node status')
-
-        file.close()
 
     def download_url(self, url, output_path):
         with DownloadProgressBar(unit='B', unit_scale=True,
@@ -73,6 +77,7 @@ class AlgoDeploy:
             if "stable" in release["tag_name"]:
                 return release["tag_name"]
 
+    # https://stackoverflow.com/a/57970619
     def cmd(self, cmd_str):
         process = subprocess.Popen(
             cmd_str,
@@ -91,6 +96,34 @@ class AlgoDeploy:
 
             if realtime_output:
                 print(realtime_output.strip(), flush=True)
+
+    def msys_cmd(self, cmd_str):
+        cmd_str = cmd_str.replace("\\", "/")
+        cmd_str = cmd_str.replace("C:", "/c")
+
+        self.cmd(f'C:\\msys64\\usr\\bin\\env MSYSTEM=MINGW64 /usr/bin/bash -lc "{cmd_str}"')
+
+    def build_from_source(self, tag):
+        cmd_function = self.cmd
+
+        if platform.system() == "Windows":
+            cmd_function = self.msys_cmd
+            cmd_function("sed -i 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf")
+            cmd_function("pacman -S --disable-download-timeout --noconfirm --needed mingw-w64-x86_64-go")
+
+        tarball_path = Path.joinpath(self.download_dir, f'{tag}.tar.gz')
+        self.download_url(f'https://github.com/algorand/go-algorand/archive/{tag}.tar.gz', tarball_path)
+        
+        tarball = tarfile.open(tarball_path)
+        src_dir = Path.joinpath(self.download_dir, Path(tarball.getnames()[0]).name)
+        shutil.rmtree(path=src_dir, ignore_errors=True)
+
+        tarball.extractall(path=self.download_dir)
+        tarball.close()
+        
+        cmd_function(f"cd {src_dir} && GOPATH=$HOME/go ./scripts/configure_dev.sh")
+        cmd_function(f"cd {src_dir} && GOPATH=$HOME/go make")
+        cmd_function(f"cd {src_dir} && mkdir -p ../../localnet/bin && cp $HOME/go/bin/* ../../localnet/bin/")
 
 if __name__ == "__main__":
     AlgoDeploy()
